f1475d43bde0a05266ecfb2bc474b4bc49148dc8
vishnuscse <142710343+vishnuscse@users.noreply.github.com>
2023-08-20 11:20:47 +0530

    Add files via upload
    Day 1

---
+++ day1/HelloWorld.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is Vishnu's First Code!! Welcome");
+	}
+}
---
+++ day1/Information.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Information {
+	
+	public static void main(String[] args) {
+		
+		int weight = 89;
+		boolean hadTiffin = false;
+		long cardnumber = 347578789487923l;
+		float height = 6.11f;
+		short nym = 1;
+		byte byt = 0;
+		boolean hadBf = true;
+		String cardmodel = "Renault Kwid";
+		
+		System.out.println("My Weight is:" +weight);
+		System.out.println("Had Tiffin: :" +hadTiffin);
+		System.out.println("My Card Number is " +cardnumber);
+		System.out.println("My Weight is " +weight +"  My Height is :" +height);
+		System.out.println("My Car Model is " +cardmodel);
+		System.out.println("Short " +nym);
+		System.out.println("BYTE " +byt);
+		System.out.println("Had BreakFast " +hadBf);
+		
+
+		
+	}
+
+}
